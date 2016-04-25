2f3e44e142422453957e3a0a6c1a76c53409d1b4
songyongju <bbq7558@naver.com>
2016-04-25 17:39:32 +0900

    Revert "지역저장소로 commit 하기 !"

--- src/main/java/seNex/Calculator.java
+++ src/main/java/seNex/Calculator.java
@@ -7,9 +7,6 @@ public class Calculator {
 	public void add(int x, int y) {
 		result = x+y;
 	}
-	public void sub(int x, int y) {
-		result = x-y;
-	}
 	
 	public int getResult() {
 		return result;
